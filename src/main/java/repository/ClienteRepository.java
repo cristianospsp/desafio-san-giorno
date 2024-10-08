dd346eab2def5f4da58ff5b484ff784df2ad46b7
Cristiano de Oliveira <cristianospsp@gmail.com>
2024-10-08 16:52:36 -0300

    Ajuste no clienteRepository

--- src/main/java/repository/ClienteRepository.java
+++ src/main/java/repository/ClienteRepository.java
@@ -3,6 +3,8 @@ package repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
+import model.Cliente;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
     Optional<Cliente> findByEmail(String email);
 }
