595a1bb2d83e12183462a0229602df5661795d27
Cristiano de Oliveira <cristianospsp@gmail.com>
2024-10-08 16:35:42 -0300

    Create ClienteRepository.java
    implementado metodo findByemail

---
+++ src/main/java/repository/ClienteRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByEmail(String email);
+}
