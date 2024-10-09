3459ed22040773882ebcf01f1be5bb3454042a3c
Cristiano de Oliveira <cristianospsp@gmail.com>
2024-10-09 11:00:45 -0300

    Criada classe Cobranca

---
+++ src/main/java/model/Cobranca.java
@@ -0,0 +1,13 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Cobranca {
+
+    private String codigo;
+    private double valorPago;
+
+}
