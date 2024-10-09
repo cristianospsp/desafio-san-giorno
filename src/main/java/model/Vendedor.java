ec5f91e6989053e2a1d1afc4eff95b8da11b0b1e
Cristiano de Oliveira <cristianospsp@gmail.com>
2024-10-09 11:14:03 -0300

    criado src/main/java/config/SwaggerConfig.java

--- src/main/java/model/Vendedor.java
+++ src/main/java/model/Vendedor.java
@@ -1,4 +1,13 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Vendedor {
+
+    private String codigo;
+    private String nome;
+
 }
--- src/main/java/repository/VendedorRepository.java
+++ src/main/java/repository/VendedorRepository.java
@@ -1,4 +1,11 @@
 package repository;
 
-public class VendedorRepository {
+import model.Vendedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VendedorRepository extends JpaRepository<Vendedor, String> {
+    Optional<Vendedor> findByCodigo(String codigo);
+
 }
