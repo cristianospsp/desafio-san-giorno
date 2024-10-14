87b913cb69b42f0104bf11e23dc287525f7ab71e
root <root@kali.cristiano>
2024-10-14 05:26:38 -0300

    adicionado mapeamento: @Entity

--- src/main/java/model/Cobranca.java
+++ src/main/java/model/Cobranca.java
@@ -1,20 +1,40 @@
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cobranca {
 
-    private String codigo;
-    private double valorPago;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public Cobranca(String codigo, double valorPago) {
+    private String codigo;
+    private double valorOriginal;
+
+    public Cobranca() { }
+
+    public Cobranca(String codigo, double valorOriginal) {
         this.codigo = codigo;
-        this.valorPago = valorPago;
+        this.valorOriginal = valorOriginal;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getCodigo() {
         return codigo;
     }
 
-    public double getValorPago() {
-        return valorPago;
+    public double getValorOriginal() {
+        return valorOriginal;
+    }
+
+    public void setValorOriginal(double valorOriginal) {
+        this.valorOriginal = valorOriginal;
     }
 }
